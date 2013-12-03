aaa04ceba26730ce96c3c53e05c49f58c4a76e77
vinicius.acioly@hotmail.com <vinicius.acioly@hotmail.com@8d32c395-fe41-f560-1b3a-b2a90acc22bf>
2013-12-03 00:59:42 +0000

    Sprint 1 - DESIGN: Criação do layout da tela da Lista de Notificações

--- trunk/gen/com/dizae/R.java
+++ trunk/gen/com/dizae/R.java
@@ -27,33 +27,38 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000f;
+        public static final int action_settings=0x7f080012;
         public static final int btCadastrar=0x7f080008;
         public static final int btCancelar=0x7f080009;
         public static final int btLogin=0x7f08000a;
-        public static final int btVoltar=0x7f08000e;
-        public static final int button2=0x7f08000d;
-        public static final int email=0x7f08000b;
+        public static final int btVoltar=0x7f08000d;
+        public static final int button2=0x7f08000c;
+        public static final int email=0x7f080011;
         public static final int etConSenha=0x7f080007;
+        public static final int etDescricao=0x7f08000e;
         public static final int etEmail=0x7f080004;
         public static final int etEndereco=0x7f080003;
         public static final int etNome=0x7f080001;
         public static final int etSenha=0x7f080006;
+        public static final int expandableListView1=0x7f080010;
         public static final int senha=0x7f080002;
-        public static final int spinner1=0x7f08000c;
+        public static final int spinner1=0x7f08000b;
+        public static final int svListaRecl=0x7f08000f;
         public static final int tvProblema=0x7f080005;
     }
     public static final class layout {
         public static final int activity_cadastro=0x7f030000;
         public static final int activity_home=0x7f030001;
-        public static final int activity_login=0x7f030002;
-        public static final int activity_main=0x7f030003;
+        public static final int activity_lista_recl=0x7f030002;
+        public static final int activity_login=0x7f030003;
+        public static final int activity_main=0x7f030004;
     }
     public static final class menu {
         public static final int cadastro=0x7f070000;
         public static final int home=0x7f070001;
-        public static final int login=0x7f070002;
-        public static final int main=0x7f070003;
+        public static final int lista_recl=0x7f070002;
+        public static final int login=0x7f070003;
+        public static final int main=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -77,6 +82,8 @@ public final class R {
         public static final int reportar=0x7f050015;
         public static final int senha=0x7f050009;
         public static final int spCategorias=0x7f050012;
+        public static final int svListaRecl=0x7f050017;
+        public static final int title_activity_lista_recl=0x7f050016;
         public static final int tvProblema=0x7f050011;
     }
     public static final class style {
