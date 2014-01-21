d20151475e7ec805e01aa59bd5355d0c521044f9
brobertofs <brobertofs@gmail.com>
2014-01-21 17:44:38 +0000

    Correção de bugs, redimensionamento de imagens

--- src/com/dizae/LoginActivity.java
+++ src/com/dizae/LoginActivity.java
@@ -2,14 +2,29 @@ package com.dizae;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+	
+	Button btLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		btLogin = (Button) findViewById(R.id.btLogin);
+		
+
+		btLogin.setOnClickListener(new View.OnClickListener() {                 
+			@Override
+			public void onClick(View arg0) {
+				chamarHomeRank();                         
+			}
+		});
 	}
 
 	@Override
@@ -17,6 +32,11 @@ public class LoginActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
+	}
+	
+	public void chamarHomeRank () {
+		Intent intent = new Intent(this, HomeRankingActivity.class);        
+		startActivity (intent);   
 	}
 
 }
