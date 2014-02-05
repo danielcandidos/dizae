3d94f686fed4aadc318826ada5577a647505c984
brobertofs <brobertofs@gmail.com>
2014-02-05 20:56:41 +0000

    Design - Sprint 3 - Spinner para Categoria e para o Tipo de Exibição - Verificar HomeRankingActivity.class e RepProblActivity.class

--- src/com/dizae/HomeRankingActivity.java
+++ src/com/dizae/HomeRankingActivity.java
@@ -1,15 +1,56 @@
 package com.dizae;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class HomeRankingActivity extends Activity {
-
+	
+	private Spinner aspn;
+	private List<String> Lista = new ArrayList<String>();
+	private String tipo;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_ranking);
+		
+		Lista.add("Mais recentes");
+		Lista.add("Seu bairro");
+		Lista.add("Ranking 10");
+				
+		//Identifica o Spinner no layout
+		aspn = (Spinner) findViewById(R.id.spinner1);
+		//Cria um ArrayAdapter usando um padrão de layout da classe R do android, passando o ArrayList nomes
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, Lista);
+		ArrayAdapter<String> spinnerArrayAdapter = arrayAdapter;
+		spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+		aspn.setAdapter(spinnerArrayAdapter);
+ 
+		//Método do Spinner para capturar o item selecionado
+		aspn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+ 
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View v, int posicao, long id) {
+				//pega nome pela posição
+				tipo = parent.getItemAtPosition(posicao).toString();
+				//imprime um Toast na tela com o nome que foi selecionado
+				Toast.makeText(HomeRankingActivity.this, "Nome Selecionado: " + tipo, Toast.LENGTH_LONG).show();
+			}
+ 
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+ 
+			}
+		});
+		
 	}
 
 	@Override
