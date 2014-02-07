ec81c855930077b39166d5597fc7b1a304980016
mauriokk <mauriokk@gmail.com>
2014-02-07 17:36:30 +0000

    Sprint 4 - Merge de sidebar e login/cadastro - Realizado merge manual do sidebar e o método de login/cadastro

--- src/com/dizae/content/HomeFragment.java
+++ src/com/dizae/content/HomeFragment.java
@@ -1,21 +1,16 @@
 package com.dizae.content;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.dizae.CadastroActivity;
 import com.dizae.LoginActivity;
 import com.dizae.R;
-import com.dizae.UsuarioActivity;
 import com.dizae.database.UserDAO;
 
 public class HomeFragment extends Fragment {
