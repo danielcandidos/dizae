915c93aa57369a5e4e8909379e9d1f9119cd5815
mauriokk@gmail.com <mauriokk@gmail.com@8d32c395-fe41-f560-1b3a-b2a90acc22bf>
2014-02-08 14:52:57 +0000

    Sprint4 - Funções dos itens da Spinner - reparação do método de retorno ao clicar em item da spinner

--- trunk/src/com/dizae/HomeRankingActivity.java
+++ trunk/src/com/dizae/HomeRankingActivity.java
@@ -15,47 +15,41 @@ import android.widget.Toast;
 public class HomeRankingActivity extends Activity {
 	
 	private Spinner aspn;
-	private List<String> Lista = new ArrayList<String>();
-	private String tipo;
+	private List<String> listaSelecione = new ArrayList<String>();
+	private String opcao;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_ranking);
 		
-		Lista.add("Selecione");
-		Lista.add("Mais recentes");
-		Lista.add("Seu bairro");
-		Lista.add("Ranking 10");
+		listaSelecione.add("Selecione");
+		listaSelecione.add("Mais recentes");
+		listaSelecione.add("Seu bairro");
+		listaSelecione.add("Ranking 10");
 				
 		//Identifica o Spinner no layout
 		aspn = (Spinner) findViewById(R.id.spinner1);
-		//Cria um ArrayAdapter usando um padrão de layout da classe R do android, passando o ArrayList nomes
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, Lista);
+		//Cria um ArrayAdapter usando um padrão de layout da classe R do android, passando o ArrayList listaSelecione
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listaSelecione);
 		ArrayAdapter<String> spinnerArrayAdapter = arrayAdapter;
 		spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 		aspn.setAdapter(spinnerArrayAdapter);
- 
+		
 		//Método do Spinner para capturar o item selecionado
 		aspn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
- 
-			public void onItemSelected1(AdapterView<?> parent, View v, int posicao, long id) {
-				//pega nome pela posição
-				tipo = parent.getItemAtPosition(posicao).toString();
-				//imprime um Toast na tela com o nome que foi selecionado
-				Toast.makeText(HomeRankingActivity.this, "Nome Selecionado: " + tipo, Toast.LENGTH_LONG).show();
+		 
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View v, int posicao, long id) {
+				//pega opcao pela posição
+				opcao = parent.getItemAtPosition(posicao).toString();
+				//imprime um Toast na tela com o opcao que foi selecionado
+				Toast.makeText(HomeRankingActivity.this, "opcao Selecionado: " + opcao, Toast.LENGTH_LONG).show();
 			}
- 
+		
 			@Override
 			public void onNothingSelected(AdapterView<?> parent) {
- 
-			}
-
-			@Override
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-				// TODO Auto-generated method stub
-				
+		 
 			}
 		});
 		
