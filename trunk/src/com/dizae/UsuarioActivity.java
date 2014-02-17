a0d7da782b287b545cb5861611449297e0d75e88
danielcands@gmail.com <danielcands@gmail.com@8d32c395-fe41-f560-1b3a-b2a90acc22bf>
2014-02-17 21:51:49 +0000

    Sprint 5 - [Bruno] Sidebar - Complemento dos metodos da sidebar

--- trunk/src/com/dizae/UsuarioActivity.java
+++ trunk/src/com/dizae/UsuarioActivity.java
@@ -2,21 +2,45 @@ package com.dizae;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class UsuarioActivity extends Activity {
-
+	
+	Button editButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_usuario);
-	}
+		
+		editButton=(Button)findViewById(R.id.button1);
+		editButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				chamaeditarUsuario();
+				
+			
+			}
+		});
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.usuario, menu);
-		return true;
 	}
+		public void chamaeditarUsuario(){
+			Intent chama = new Intent(this, EditarUsuarioActivity.class);
+			chama.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(chama);
+		}
+		
+		@Override
+		protected void onDestroy() {
+			super.onDestroy();
+			
+		}
+
+
 
 }
